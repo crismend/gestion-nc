4016ec6128027ae1ed8185dc4a0c9d5cfba4d350
Cristian Mendoza <cfmendoza.audit@gmail.com>
2025-05-17 18:25:18 +0200

    Fix: eliminada referencia conflictiva a styles.css en index.html

--- frontend-nc/src/main.jsx
+++ frontend-nc/src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import './styles.css'
 import App from './App.jsx'
 import { AuthProvider } from './context/AuthContext.jsx'
 import { BrowserRouter as Router } from 'react-router-dom'
