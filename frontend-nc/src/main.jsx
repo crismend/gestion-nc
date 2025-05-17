6a89dda095890ff9e96731775c96709ff45dccb1
Cristian Mendoza <cfmendoza.audit@gmail.com>
2025-05-17 19:04:40 +0200

    Forzar eliminación de index.css y corrección fina-1l

--- frontend-nc/src/main.jsx
+++ frontend-nc/src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './styles.css'
+import './index.css'
 import App from './App.jsx'
 import { AuthProvider } from './context/AuthContext.jsx'
 import { BrowserRouter as Router } from 'react-router-dom'
